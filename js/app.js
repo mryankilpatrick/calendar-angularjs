e76919e848a5a22fe54e4fbf02298e8b28533cec
mryankilpatrick <markryandesigner@gmail.com>
2015-02-23 00:12:21 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-var app = angular.module('CalendarApp', []);
+var app = angular.module('CalendarApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'DayController',
+			templateUrl: 'views/day.html'
+		});
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+// In app.js, define the app's routes:
+// Use app.config() and $routeProvider to set up the routes.
+// Map the URL / to the controller DayController and the template views/day.html. We'll write these next.
+// Otherwise, redirect to /
+
+
+// Example
+
+// var app = angular.module('GalleryApp', ['ngRoute']);
+
+// app.config(function ($routeProvider) {
+// 	$routeProvider
+// 		.when('/', {
+// 			controller: 'HomeController',
+// 			templateUrl: 'views/home.html'
+// 		})
+// 		.when('/photos/:id', {
+// 			controller: 'PhotoController',
+// 			templateUrl: 'views/photo.html'
+// 		})
+// 		.otherwise({
+// 			redirectTo: '/'
+// 		});
+// });
